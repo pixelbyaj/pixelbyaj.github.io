1d7759ce256d669036c093a0a26cc552669ae3f5
Abhishek Joshi <abhishek2185@live.in>
2021-04-21 13:55:24 +0530

    Kids playarea

---
+++ app/kids/alphabet.js
@@ -0,0 +1,37 @@
+(() => {
+    const alphabet = document.getElementById("alphabet");
+    alphabet.value = "";
+    alphabet.focus();
+    let code = 0;
+
+    const alphaCheck = (e) => {
+        return !((e.which >= 65 && e.which <= 90 || e.which >= 97 && e.which <= 122) || e.which === 13 || e.key.length === 1);
+    }
+    const getRandomNumber = (min, max) => {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+
+    }
+    const addRandomColor = (ele) => {
+        var r = getRandomNumber(0, 255);
+        var g = getRandomNumber(0, 255);
+        var b = getRandomNumber(0, 255);
+        ele.style.color = `rgb(${r},${g},${b})`;
+    }
+
+    alphabet.addEventListener("keypress", (e) => {
+        if (alphaCheck(e)) {
+            e.preventDefault();
+        } else {
+            if (e.which <= 90) {
+                e.target.value += `${e.key.toLocaleLowerCase()}`;
+            } else {
+                e.target.value += `${e.key.toLocaleUpperCase()}`;
+            }
+            setTimeout(() => {
+                e.target.value += " ";
+            })
+            addRandomColor(e.target);
+        }
+    }, false);
+
+})();
